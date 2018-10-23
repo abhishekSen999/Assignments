09ad4c3548df9124f67363a404b3afe114a93fc3
Abhishek Sen <abhishek.mcs18.du@gmail.com>
2018-10-23 23:57:37 +0530

    started sudoku

---
+++ MCS102/GUI_Sudoku.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 23 23:49:31 2018
+
+@author: abhishek sen
+"""
+import Sudoku_solver as sudoku
+import tkinter as tkr
+GUI_Sudoku()
+
+
+
+class GUI_Sudoku(object):
+    def __init__(self):
+        top= tkr.Tk()
+        
+        top.mainloop() 
+        
+        
+        
+        
+        
---
+++ MCS102/Sudoku_solver.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 23 23:27:44 2018
+
+@author: abhishek sen
+"""
+
+class Sudoku_solver(object):
+    def generate(self):
+        
+        
+        
+        
+        
