67172434bd7cfd9e8e0f4aaa0250130ff7e89bc5
Abhishek Sen <abhishek.sen999@gmail.com>
2019-03-24 04:11:48 +0530

    using BaseManager

--- MCS202/getblock_implimentation/DriverProcess.py
+++ MCS202/getblock_implimentation/DriverProcess.py
@@ -11,31 +11,34 @@ import BufferHeader
 import BufferManagement
 import os
 import myProcess
-def main():
 
-    lengthOfHashQ=4
-    freeListSize=20
-    maxNoOfBlocks=50
 
-    #using shared memory objects using BaseManager from multiprocessing library
-    BaseManager.register('HashQueue',HashQueue.HashQueue)
-    BaseManager.register('FreeList',FreeList.FreeList)
-    manager=BaseManager()
-    manager.start()
+lengthOfHashQ=4
+freeListSize=20
+maxNoOfBlocks=50
 
-    hashQ=manager.HashQueue(lengthOfHashQ)
-    freeList=manager.FreeList(freeListSize)
-    lock=multiprocessing.Lock()
-    p1=multiprocessing.Process(target=myProcess.process,args=(hashQ,freeList,lock,maxNoOfBlocks,))
-    p2=multiprocessing.Process(target=myProcess.process,args=(hashQ,freeList,lock,maxNoOfBlocks,))
-    p3=multiprocessing.Process(target=myProcess.process,args=(hashQ,freeList,lock,maxNoOfBlocks,))
-    p1.start()
-    p2.start()
-    p3.start()
-    p1.join()
-    p2.join()
-    p3.join()
-    print("end")
+#using shared memory objects using BaseManager from multiprocessing library
+BaseManager.register('HashQueue',HashQueue.HashQueue)
+BaseManager.register('FreeList',FreeList.FreeList)
+manager=BaseManager()
+manager.start()
+
+hashQ=manager.HashQueue(lengthOfHashQ)
+freeList=manager.FreeList(freeListSize)
+lock=multiprocessing.Lock()
+p1=multiprocessing.Process(target=myProcess.process,args=(hashQ,freeList,lock,maxNoOfBlocks,))
+p2=multiprocessing.Process(target=myProcess.process,args=(hashQ,freeList,lock,maxNoOfBlocks,))
+p3=multiprocessing.Process(target=myProcess.process,args=(hashQ,freeList,lock,maxNoOfBlocks,))
+p1.start()
+p2.start()
+p3.start()
+p1.join()
+p2.join()
+p3.join()
+print("end")
+
+
+
 
 
 
