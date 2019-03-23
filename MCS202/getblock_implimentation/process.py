7e6149baf6c16fa7916ecf62915ce9b6da6da798
Abhishek Sen <abhishek.sen999@gmail.com>
2019-03-24 03:39:47 +0530

    process complete

--- MCS202/getblock_implimentation/process.py
+++ MCS202/getblock_implimentation/process.py
@@ -6,30 +6,49 @@ import FreeList
 import BufferHeader
 import BufferManagement
 import os
+import random
 
 
-def sudoOperation():
+def sudoOperation(buffer):
     """
-    0-write operation followed by marking buffer delayed write and unlocking
-    1-work done(disk read is done if buffer was not initially valid), validate buffer ,unlock buffer
+    0-write operation followed by marking buffer delayed write 
+    1-work done(disk read is done if buffer was not initially valid), validate buffer 
     2-mark buffer invalid
     3-
-
     """
+    time.sleep(2) #simulating an operation
+    operation=random.randint(0,2)
+    if(operation==0):
+        buffer.setDelayedWriteBit()
+    elif(operation==1):
+        buffer.setValidBit()
+    elif(operation==2):
+        buffer.clearValidBit()
 
-def sudoBRelease(hashQ,freeList,lock,block):
+
+def sudoBRelease(hashQ,freeList,lock,buffer):
+    lock.acquire()
+    if(buffer.isValid()):
+        freeList.addToFreeListEnd(buffer)
+    else:
+        freeList.addToFreeListFirst(buffer)
+
+    buffer.clearLockedBit()
+    lock.release()
 
 
 def process(hashQ,freeList,lock,maxNoOfBlocks):
     
     while(True):
-        time.sleep(2)#process will request a random block after every 1 second
+        time.sleep(2)#process will request a random block after every 2 second
         requestedBlock=random.randint(0,maxNoOfBlocks-1)
         print("process : ",os.getpid()," has requested block number : ",requestedBlock)
         recievedBuffer=BufferManagement.getBlock(requestedBlock,lock,hashQ,freeList)
         print("process : ",os.getpid(),"recieved buffer: ",recievedBuffer.getBlockNumber())
-        time.sleep(2)
-        sudoOperation(buffer)
+        
+        sudoOperation(recievedBuffer)
+
+        
 
 
 
